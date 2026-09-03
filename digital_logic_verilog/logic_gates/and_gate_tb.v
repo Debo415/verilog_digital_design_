11f284a0314529f24fd481d933a33eea917932e5
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-03 13:02:27 +0530

    Update and_gate_tb.v

--- digital_logic_verilog/logic_gates/and_gate_tb.v
+++ digital_logic_verilog/logic_gates/and_gate_tb.v
@@ -1,1 +1,23 @@
+module and_gate_tb;
+
+reg A, B;
+wire Y;
+
+and_gate uut (
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $monitor("A=%b B=%b Y=%b", A, B, Y);
+
+    A = 0; B = 0;
+    #10 A = 0; B = 1;
+    #10 A = 1; B = 0;
+    #10 A = 1; B = 1;
+    #10 $finish;
+end
+
+endmodule
 
