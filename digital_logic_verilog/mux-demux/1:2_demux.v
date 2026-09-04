faa2216e50bc6006eb8d884f81d4014ed6dda94f
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-04 14:38:30 +0530

    Refactor demux_1to2 implementation
    Refactor 1:2 demultiplexer to use always block and case statement.

--- digital_logic_verilog/mux-demux/1:2_demux.v
+++ digital_logic_verilog/mux-demux/1:2_demux.v
@@ -1,6 +1,13 @@
-module 12_demux_df(Y,I,S);
-  input I,S;
-  output [1:0] Y;
-  assign Y[0]= I & ~S;
-  assign Y[0]= I & S;
-endmodule
+module demux_1to2(
+    input D,
+    input S,
+  output reg [1:0] Y);
+  always @(*) begin
+    Y = 2'b00;
+
+    case (S)
+        1'b0: Y[0] = D;
+        1'b1: Y[1] = D;
+    endcase
+end
+
