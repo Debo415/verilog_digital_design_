16bec88b2fd3b3e84475ad8a9ca1df3149ddc850
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-05 12:56:52 +0530

    Update and rename testbench_mux to testbench_mux.v

---
+++ digital_logic_verilog/mux-demux/testbench_mux.v
@@ -0,0 +1,10 @@
+module all_mux_tb;
+  reg [1:0] I1, [3:0] I2, S;
+  wire Y2;
+  wire Y4;
+  mux_21 MUX21( .S(S), .I(I), .Y(Y2));
+  mux_41 MUX41( .S(S), .I(I), .Y(Y4));
+
+  initial begin
+    $monitor("S=%b I=%b | Y21=%b Y41=%b ", S,I,Y2,Y4);
+    
