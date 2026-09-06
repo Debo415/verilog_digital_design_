46f4ed43f121d6c85f666a738f658ceba0c34ee0
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-06 12:40:43 +0530

    Update encoder-decoder.v

--- digital_logic_verilog/encoder-decoder/encoder-decoder.v
+++ digital_logic_verilog/encoder-decoder/encoder-decoder.v
@@ -1,1 +1,7 @@
-
+module encode_42(Y4,V4,I4);
+  input [3:0] I4;
+  output [1:0] Y4;
+  output V4;
+  assign Y4 = {I4[3] | I4[2] , I4[3] | I4[1]};
+  assign V4= I4[3] | I4[2] |I4[1] | I4[0] ;
+endmodule
