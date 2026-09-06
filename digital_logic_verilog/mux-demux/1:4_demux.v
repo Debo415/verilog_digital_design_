7a4c896fe1ddc9c948faf08556ec9315b6750d78
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-06 11:07:08 +0530

    Update 1:4_demux.v

--- digital_logic_verilog/mux-demux/1:4_demux.v
+++ digital_logic_verilog/mux-demux/1:4_demux.v
@@ -1,27 +1,19 @@
-module demux_1to4(
-    input D,
-    input [1:0] S,
-    output reg Y0,
-    output reg Y1,
-    output reg Y2,
-    output reg Y3
+module demux_14(
+    input I4,
+    input [1:0] S4,
+    output reg [3:0] Y4
 );
 
 always @(*) begin
 
     // Default: all outputs OFF
-    Y0 = 0;
-    Y1 = 0;
-    Y2 = 0;
-    Y3 = 0;
+    Y4 = 2'b00;
 
-    case(S)
-        2'b00: Y0 = D;
-        2'b01: Y1 = D;
-        2'b10: Y2 = D;
-        2'b11: Y3 = D;
+    case(S4)
+        2'b00: Y4[0] = I4;
+        2'b01: Y4[1] = I4;
+        2'b10: Y4[2] = I4;
+        2'b11: Y4[3] = I4;
     endcase
-
 end
-
 endmodule
