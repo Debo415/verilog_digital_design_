6aa7d09d5cc69d8ec926e39f8ef425fa1b64c282
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-06 09:18:05 +0530

    Update testbench_mux.v

--- digital_logic_verilog/mux-demux/testbench_mux.v
+++ digital_logic_verilog/mux-demux/testbench_mux.v
@@ -1,20 +1,28 @@
 module all_mux_tb;
-  reg [1:0] I1, [3:0] I2, S;
+  reg [1:0] I2;
+  reg S2;
   wire Y2;
+
+  reg [3:0] I4;
+  reg S4;
   wire Y4;
-  mux_21 MUX21( .S(S), .I(I), .Y(Y2));
-  mux_41 MUX41( .S(S), .I(I), .Y(Y4));
+  integer i;
+  mux_21 MUX21( .S2(S2), .I2(I2), .Y2(Y2));
+  mux_41 MUX41( .S4(S4), .I4(I4), .Y4(Y4));
 
   initial begin
-    $monitor("S=%b I=%b | Y21=%b Y41=%b ", S,I,Y2,Y4);
-    reg [3:0] A;
-        integer i;
-        initial begin
-            for(i=0; i<16; i=i+1) begin
-                A=i;
-                #10;
-            end
-        end
-        $finish;
+    for(i=0; i<8; i=i+1) begin
+      {S2,I2}=i;
+      #10;
+      $monitor("S=%b I=%b | Y21=%b ", S2,I2,Y2);
+    end
+
+    for(i=0; i<64; i=i+1) begin
+      {S4,I4}=i;
+      #10;
+      $monitor("S=%b I=%b | Y41=%b ", S4,I4,Y4);
     end
     
+    $finish;
+  end
+endmodule
