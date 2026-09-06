e551b89aa25b40d4511ca7b45efa47f200107c7a
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-06 11:08:10 +0530

    Rename demux module and update inputs/outputs

--- digital_logic_verilog/mux-demux/1:2_demux.v
+++ digital_logic_verilog/mux-demux/1:2_demux.v
@@ -1,13 +1,13 @@
-module demux_1to2(
-    input D,
-    input S,
-  output reg [1:0] Y);
+module demux_12(
+    input I2,
+    input S2,
+    output reg [1:0] Y2);
   always @(*) begin
-    Y = 2'b00;
+    Y2 = 2'b00;
 
-    case (S)
-        1'b0: Y[0] = D;
-        1'b1: Y[1] = D;
+      case (S2)
+          1'b0: Y2[0] = I2;
+          1'b1: Y2[1] = I2;
     endcase
 end
 
