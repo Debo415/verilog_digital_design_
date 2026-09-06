8b6f361df58102baac5f296bb1122eb8b38441be
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-06 12:43:59 +0530

    Implement 2-to-4 decoder module in Verilog

--- digital_logic_verilog/encoder-decoder/2to4_decoder.v
+++ digital_logic_verilog/encoder-decoder/2to4_decoder.v
@@ -1,1 +1,7 @@
-
+module decoder_24(input a, b, en,
+  output y0,y1,y2,y3);
+  assign y0 = (~a) & (~b) & en;
+  assign y1 = (~a) & b & en;
+  assign y2 = a & (~b) & en;
+  assign y3 = a & b & en;
+endmodule
