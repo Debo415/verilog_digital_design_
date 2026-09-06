8637b7b24d23260809ac93a0a62f1ca14f7fa223
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-06 12:52:34 +0530

    Refactor 4to2_encoder module input/output names

--- digital_logic_verilog/encoder-decoder/4to2_encoder.v
+++ digital_logic_verilog/encoder-decoder/4to2_encoder.v
@@ -1,7 +1,7 @@
-module encode_42(Y4,V4,I4);
-  input [3:0] I4;
-  output [1:0] Y4;
-  output V4;
-  assign Y4 = {I4[3] | I4[2] , I4[3] | I4[1]};
-  assign V4= I4[3] | I4[2] |I4[1] | I4[0] ;
+module encode_42(Y,V,I);
+  input [3:0] I;
+  output [1:0] Y;
+  output V;
+  assign Y = {I[3] | I[2] , I[3] | I[1]};
+  assign V= I[3] | I[2] |I[1] | I[0] ;
 endmodule
