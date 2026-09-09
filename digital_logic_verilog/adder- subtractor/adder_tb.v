f6cdc6fdfc46f4d4c3c1f6685ca5d72a53dfc927
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-09 14:29:24 +0530

    Create testbench for full adder module
    Added a testbench for the full adder module to verify functionality with various input combinations.

--- digital_logic_verilog/adder- subtractor/adder_tb.v
+++ digital_logic_verilog/adder- subtractor/adder_tb.v
@@ -1,1 +1,17 @@
-
+module full_adder_testbench;
+  reg a,b,c;
+  wire sum,carry;
+  full_adder uut(.sum(sum),.carry(carry),.a(a),.b(b),.cin(c));
+  initial begin
+    $monitor ("A = %b, B = %b, C = %b CARRY = %b, SUM = %b.",a,b,c,carry,sum);
+    a = 0; b = 0; c = 0;
+    #100 a = 0; b = 0; c = 1;
+    #100 a = 0; b = 1; c = 0;
+    #100 a = 0; b = 1; c = 1;
+    #100 a = 1; b = 0; c = 0;
+    #100 a = 1; b = 0; c = 1;
+    #100 a = 1; b = 1; c = 0;
+    #100 a = 1; b = 1; c = 1;
+    #100;
+    $stop;
+  end
