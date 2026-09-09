d4c53681e2635aa4ba42558c12bdfa47dd2bc9ec
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-09 16:49:24 +0530

    Update 1-bit_subtractor.v

--- digital_logic_verilog/adder- subtractor/1-bit_subtractor.v
+++ digital_logic_verilog/adder- subtractor/1-bit_subtractor.v
@@ -1,1 +1,6 @@
-
+module FullSubtractor( diff,Bout, a, b, Bin);
+  output diff, Bout;
+  input a, b, Bin;
+  assign diff = a ^ b ^ Bin;
+  assign Bout = ((~a)&b) | (Bin&(a^~b)); //xnor symbol ^~ or ~^
+endmodule
