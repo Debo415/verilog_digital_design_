3a6f13bbf80a9c5b6503f43586e982925cc2d640
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-12 13:11:34 +0530

    Update RCA_tb.v

--- digital_logic_verilog/combinational_circuit/adder- subtractor/RCA_adder/RCA_tb.v
+++ digital_logic_verilog/combinational_circuit/adder- subtractor/RCA_adder/RCA_tb.v
@@ -4,14 +4,13 @@ module RippleCarryAdder_4bit_testbench;
   wire cout;
   RippleCarryAdder_4bit uut(.A(A), .B(B), .sum(sum), .cout(cout));
   initial begin
-    $random;
     repeat (10) begin
       A = $urandom_range(0,15);
       B = $urandom_range(0,15);
-      $display("A = %d, B = %d", $unsigned(A), $unsigned(B));
-      #100;
-      $display("Sum = %d, Carry = %d", $unsigned(sum), $unsigned(cout));
+      $display("A = %d, B = %d", A, B);
+      #10;
+      $display("Sum = %d, Carry = %d", sum, cout);
     end
-    $stop;
+    $finish;
   end
 endmodule
