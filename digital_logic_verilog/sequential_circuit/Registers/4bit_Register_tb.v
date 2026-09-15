d788acb0e5df3ed2520c608e9dec45e0e7881fe1
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-15 13:07:04 +0530

    Update 4bit_Register_tb.v

--- digital_logic_verilog/sequential_circuit/Registers/4bit_Register_tb.v
+++ digital_logic_verilog/sequential_circuit/Registers/4bit_Register_tb.v
@@ -1,1 +1,38 @@
+module tb;
 
+reg clk;
+reg rst;
+reg [3:0] d;
+wire [3:0] q;
+
+register_4bit uut (
+    .clk(clk),
+    .rst(rst),
+    .d(d),
+    .q(q)
+);
+
+// Clock generation
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    rst = 1;
+    d = 4'b0000;
+
+    #10 rst = 0;
+
+    #10 d = 4'b1010;
+    #10 d = 4'b1100;
+    #10 d = 4'b0011;
+    #10 d = 4'b1111;
+
+    #20 $finish;
+end
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb);
+end
+
+endmodule
