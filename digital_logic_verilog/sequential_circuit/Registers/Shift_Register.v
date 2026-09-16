76cc92f5db7a2938f3f9aca5d2cd579b9a32d0da
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-16 10:12:23 +0530

    Update Shift_Register.v

--- digital_logic_verilog/sequential_circuit/Registers/Shift_Register.v
+++ digital_logic_verilog/sequential_circuit/Registers/Shift_Register.v
@@ -1,1 +1,15 @@
+module shift_register_4bit(
+    input clk,
+    input rst,
+    input serial_in,
+    output reg [3:0] q
+);
 
+always @(posedge clk) begin
+    if (rst)
+        q <= 4'b0000;
+    else
+        q <= {serial_in, q[3:1]};
+end
+
+endmodule
