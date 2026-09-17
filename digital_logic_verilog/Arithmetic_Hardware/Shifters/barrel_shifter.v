b70336d96b921a87be079cb51dd9f2918b92b182
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-17 11:03:41 +0530

    Implement barrel shifter module in Verilog

--- digital_logic_verilog/Arithmetic_Hardware/Shifters/barrel_shifter.v
+++ digital_logic_verilog/Arithmetic_Hardware/Shifters/barrel_shifter.v
@@ -1,1 +1,15 @@
+module barrel_shifter (
+    input  [7:0] data_in,
+    input  [2:0] shift_amt,
+    input        direction,   // 0 = left, 1 = right
+    output reg [7:0] data_out
+);
 
+always @(*) begin
+    if (direction == 1'b0)
+        data_out = data_in << shift_amt;
+    else
+        data_out = data_in >> shift_amt;
+end
+
+endmodule
