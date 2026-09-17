6b449568f6568d75c4a8f5b18dc2269148018a47
Deboborni Guha Roy <311523105010@msec.edu.in>
2026-09-17 10:31:43 +0530

    Update 8-bit_sub.v

--- digital_logic_verilog/Arithmetic_Hardware/Subtractor/8-bit_sub.v
+++ digital_logic_verilog/Arithmetic_Hardware/Subtractor/8-bit_sub.v
@@ -1,1 +1,17 @@
+module subtractor_8bit (
+    input  [7:0] A,
+    input  [7:0] B,
+    output [7:0] Diff,
+    output       Borrow
+);
+
+    wire [8:0] result;
+
+    // A - B = A + (~B) + 1
+    assign result = {1'b0, A} + {1'b0, ~B} + 9'b000000001;
+
+    assign Diff   = result[7:0];
+    assign Borrow = ~result[8];
+
+endmodule
 
